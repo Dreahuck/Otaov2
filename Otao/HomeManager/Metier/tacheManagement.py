ac7866ea3005f99bbca40019287dad9e1566df3e
Dreahuck <dreahuck@gmail.com>
2021-11-20 18:35:08 +0100

    Correctif TchouCoin

--- Otao/HomeManager/Metier/tacheManagement.py
+++ Otao/HomeManager/Metier/tacheManagement.py
@@ -6,13 +6,14 @@ from django.utils import timezone
 def changer_etat_tache(tache, etat_tache,user_id):
     tache.etat_text = etat_tache
     tache.save()
-    if (tache.tchouCoin > 0):
+    if (etat_tache == "RE" and tache.tchouCoin > 0):
         user = get_object_or_404(Personne,username_text=user_id)
-        if tache.jalon_date >= timezone.now():
-            user.tchouCoinWallet += tache.tchouCoin
-        else :
-            user.tchouCoinWallet += (tache.tchouCoin/2)
-        user.save()
+        if tache.creerPar_id != user.id:
+            if tache.jalon_date >= timezone.now():
+                user.tchouCoinWallet += tache.tchouCoin
+            else :
+                user.tchouCoinWallet += (tache.tchouCoin/2)
+            user.save()
 
 def creer_tache_reward(reward, userConnected):
     tache = Tache(
